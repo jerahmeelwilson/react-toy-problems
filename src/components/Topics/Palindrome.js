4519c6b33707235e331a1526a8f06b0fa8629038
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-12-17 03:07:26 -0600

    Completed sum and palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,14 +1,72 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react'
+
+export default function Palindrome() {
+    const [userInput, setUserInput] = useState("");
+    const [palindrome, setPalindrome] = useState("");
+
+    function handleInput(event){
+        setUserInput(event.target.value)
+    }
+
+    function solveInput(event){
+        event.preventDefault();
+        let phrase = userInput;
+        let palin = [...phrase.replace(/[^a-zA-Z]/g, "")].reverse().join("") === phrase.replace(/[^a-zA-Z]/g, "");
+        setPalindrome(`${palin}`);
+        setUserInput("")
+    }
+
+    return (
+    <div className="puzzleBox palindromePB">
+        <h4>Palindrome</h4>
+        <input onChange={handleInput} value={userInput}className='inputLine'></input>
+        <button onClick={solveInput}className='confirmationButton'>Check</button>
+        <span className='resultsBox'>Palindrome: {palindrome}</span>
+    </div>
+    )
+}
+
+//import React, { Component } from 'react';
 
 // function EvenAndOdd() {
 //     return(<p>EvenAndOdd</p>)
 // }
 
-class Palindrome extends Component{
-    render(){
-        return(<p>Palindrome</p>)
-    }
-}
+// class Palindrome extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state ={
+//             userInput: "",
+//             palindrome:"",
+//         }
+//         this.handleInput = this.handleInput.bind(this);
+//         this.solveInput = this.solveInput.bind(this);
+//     }
+
+//     handleInput(event){
+//         this.setState({userInput: event.target.value});
+//     }
+
+//     solveInput(event){
+//         event.preventDefault();
+//         let phrase = this.state.userInput;
+//         let palin = [...phrase.replace(/[^a-zA-Z]/g, "")].reverse().join("") === phrase.replace(/[^a-zA-Z]/g, "");
+//         console.log(palin);
+//         this.setState({palindrome:`${palin}`});
+//         this.setState({userInput:""})
+//     }
+
+//     render(){
+//         return(
+//             <div className="puzzleBox palindromePB">
+//                 <h4>Palindrome</h4>
+//                 <input onChange={this.handleInput} value={this.state.userInput}className='inputLine'></input>
+//                 <button onClick={this.solveInput}className='confirmationButton'>Check</button>
+//                 <span className='resultsBox'>Palindrome: {this.state.palindrome}</span>
+//             </div>
+//         )
+//     }
+// }
 
 
-export default Palindrome;
+// export default Palindrome;
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,11 +1,78 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 
-export default class Sum extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+export default function Sum() {
+    const [number1, setNumber1] = useState("");
+    const [number2, setNumber2] = useState("");
+    const [sum, setSum] = useState("");
+
+    function handleInput(event) {
+        if(event.target.id === 'number1'){
+            setNumber1(event.target.value)
+        } else{
+            setNumber2(event.target.value)
+        }
     }
+
+    function solveInput(event){
+        event.preventDefault();
+        let newSum = parseInt(number1) + parseInt(number2);
+        setSum(newSum);
+    }
+
+
+    return (
+        <div className='puzzleBox sumPB'>
+            <h4>Sum</h4>
+            <input id='number1' onChange={handleInput} className='inputLine' value={number1} type='number'></input>
+            <input id ='number2' onChange={handleInput} className='inputLine' value={number2}type='number'></input>
+            <button onClick={solveInput}className='confirmationButton'>Sum</button>
+            <span className='resultsBox'>Sum: {sum}</span>
+        </div>
+    )
 }
+
+
+
+
+
+// import React, { Component } from 'react'
+
+// export default class Sum extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             number1: "",
+//             number2: "",
+//             sum: ""
+//         }
+
+//         this.handleInput = this.handleInput.bind(this);
+//         this.solveInput = this.solveInput.bind(this);
+//     }
+
+//     handleInput(event){
+//        if(event.target.id === 'number1'){
+//            this.setState({number1:event.target.value})
+//        } else{
+//            this.setState({number2:event.target.value})
+//        }
+//     }
+
+//     solveInput(event){
+//         event.preventDefault();
+//         let newSum = parseInt(this.state.number1) + parseInt(this.state.number2);
+//         this.setState({sum:newSum})
+//     }
+
+//     render() {
+//         return (
+//             <div className='puzzleBox sumPB'>
+//                 <h4>Sum</h4>
+//                 <input id='number1' onChange={this.handleInput} className='inputLine' value={this.state.number1} type='number'></input>
+//                 <input id ='number2' onChange={this.handleInput} className='inputLine' value={this.state.number2}type='number'></input>
+//                 <button onClick={this.solveInput}className='confirmationButton'>Sum</button>
+//                 <span className='resultsBox'>Sum: {this.state.sum}</span>
+//             </div>
+//         )
+//     }
+// }
